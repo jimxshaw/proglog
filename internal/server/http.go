2da2fad07164e09c59d36d281c5f240464c06693
Jim Shaw <jimxshaw@gmail.com>
2022-06-16 12:57:23 -0500

    Add http server and produce & consumer structs.

--- internal/server/http.go
+++ internal/server/http.go
@@ -1,1 +1,21 @@
 package server
+
+type httpServer struct {
+	Log *Log
+}
+
+type ProduceRequest struct {
+	Record Record `json:"record"`
+}
+
+type ProduceResponse struct {
+	Offset uint64 `json:"offset"`
+}
+
+type ConsumeRequest struct {
+	Offset uint64 `json:"offset"`
+}
+
+type ConsumeResponse struct {
+	Record Record `json:"record"`
+}

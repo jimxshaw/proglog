8fb530111d69bc6e6d4caeedac7ca470b1f9100d
Jim Shaw <jimxshaw@gmail.com>
2022-06-16 13:03:10 -0500

    Add public and private new http server methods.

--- internal/server/http.go
+++ internal/server/http.go
@@ -1,5 +1,11 @@
 package server
 
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 type httpServer struct {
 	Log *Log
 }
@@ -18,4 +24,22 @@ type ConsumeRequest struct {
 
 type ConsumeResponse struct {
 	Record Record `json:"record"`
+}
+
+func NewHttpServer(address string) *http.Server {
+	server := newHttpServer()
+	router := mux.NewRouter()
+	router.HandleFunc("/", server.handleProduce).Methods("POST")
+	router.HandleFunc("/", server.handleConsume).Methods("GET")
+
+	return &http.Server{
+		Addr:    address,
+		Handler: router,
+	}
+}
+
+func newHttpServer() *httpServer {
+	return &httpServer{
+		Log: NewLog(),
+	}
 }
